5f848134e3555da9ae398cf56bf5d18d91d7893c
sebastianogarek <sebastian.ogarek@untitledkingdom.co>
2018-10-31 17:21:44 +0100

    make it better

--- server.js
+++ server.js
@@ -200,4 +200,13 @@ io.on('connection', socket => {
   });
 })
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 server.listen(port, () => console.log(`Listening on port ${port}`))
