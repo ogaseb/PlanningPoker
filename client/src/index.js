e8742ba88c3785c6a695e45e6b48a63ff1eee7dd
sebastianogarek <oga.seb@gmail.com>
2018-11-16 17:23:18 +0100

    fixes

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import * as serviceWorker from './serviceWorker'
 import App from "./app";
 import {Provider} from "mobx-react";
 import {BrowserRouter} from "react-router-dom"
@@ -22,3 +23,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister()
