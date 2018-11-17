dc20d11576de1a673fb20e5d40d8b518a71470c0
ProPanek <oga.seb@gmail.com>
2018-11-17 20:36:08 +0100

    fixing server

--- server.js
+++ server.js
@@ -346,10 +346,12 @@ io.on("connection", socket => {
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, '/client/build')));
+  app.use(express.static(path.join({"root": __dirname}, 'client/build')));
+  console.log(__dirname)
   // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname , '/client/build', 'index.html'));
+    res.sendFile(path.join({"root": __dirname} , 'client/build', 'index.html'));
+    console.log(__dirname)
   });
 }
 
