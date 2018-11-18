5915a224bfd3e870ca5b151ac11f0636047e0b17
ProPanek <oga.seb@gmail.com>
2018-11-18 17:21:11 +0100

    fixing server

--- server.js
+++ server.js
@@ -345,11 +345,9 @@ io.on("connection", socket => {
 });
 
 if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.resolve('../app/client/build')));
-  // Handle React routing, return all requests to React app
+  app.use(express.static(path.join(__dirname, 'client/build')));
   app.get('*', function(req, res) {
-    res.sendFile(path.resolve('../app/client/build/index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
--- webpack.server.config.js
+++ webpack.server.config.js
@@ -4,6 +4,9 @@ module.exports = {
   target: "node",
   mode: "production",
   entry: "./server.js",
+  node: {
+    __dirname: true,
+  },
   output: {
     path: path.resolve(__dirname),
     filename: "lib.node.js"
